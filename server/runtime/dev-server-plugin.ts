7d551239b85d5669c8b8951e0aaf20ab7fe6d650
Davide <43080019+edivados@users.noreply.github.com>
2024-01-27 19:18:31 +0100

    move .nitro to server directory

--- server/runtime/dev-server-plugin.ts
+++ server/runtime/dev-server-plugin.ts
@@ -1,6 +1,5 @@
 export default defineNitroPlugin(async (nitroApp) => {
   const { createServer } = await import("vite");
-  console.log(import.meta.url);
   nitroApp.viteDevServer = await createServer({
     configFile: "./client/vite.config.ts",
     root: "./client",
