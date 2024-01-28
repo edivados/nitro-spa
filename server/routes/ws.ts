00095259a6c8ee7e13c48b93992bb763cc06f548
Davide <43080019+edivados@users.noreply.github.com>
2024-01-28 12:43:33 +0100

    small changes

--- server/routes/ws.ts
+++ server/routes/ws.ts
@@ -1,15 +1,13 @@
-export default lazyEventHandler(async () => {
-  return eventHandler((event) => {
-    if (event.headers.get("upgrade") !== "websocket") {
-      sendWebResponse(event, new Response(null, { status: 426 }));
-      return;
-    }
-    useWebsocketServer().handleUpgrade(
-      event.node.req, 
-      event.node.req.socket, 
-      Buffer.alloc(0), 
-      (_client, _request) => {}
-    );
-    event._handled = true;
-  });
+export default eventHandler((event) => {
+  if (event.headers.get("upgrade") !== "websocket") {
+    sendWebResponse(event, new Response(null, { status: 426 }));
+    return;
+  }
+  useWebsocketServer().handleUpgrade(
+    event.node.req, 
+    event.node.req.socket, 
+    Buffer.alloc(0), 
+    (_client, _request) => {}
+  );
+  event._handled = true;
 });
