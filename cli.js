a9d0ffa631b94c6fddd053f391244e39be6360cd
Davide <43080019+edivados@users.noreply.github.com>
2024-07-13 12:49:58 +0200

    cleanup cli

--- cli.js
+++ cli.js
@@ -1,12 +1,113 @@
 import { defineCommand, runMain } from "citty";
 import solid from "vite-plugin-solid";
 
+function clientViteConfig() {
+  return {
+    plugins: [solid()]
+  }
+}
+
+const command = defineCommand({
+  subCommands: {
+    dev: {
+      args: {
+        port: {
+          description: "port (default: 3000)",
+          default: 3000,
+        },
+      },
+      run: async ({ args: { port } }) => {
+        const { createNitro, createDevServer, prepare, build } = await import("nitropack");
+        const devHandler = await createDevHandler();
+        const nitro = await createNitro({
+          dev: true,
+          noPublicDir: true, // Leave handling of public dir to vite
+          experimental: {
+            websocket: true
+          },
+          devHandlers: [
+            {
+              route: "/",
+              handler: devHandler
+            }
+          ],
+        });
+        const server = createDevServer(nitro);
+        await server.listen(port);
+        await prepare(nitro);
+        await build(nitro);
+      },
+    },
+    build: {
+      run: async () => {
+        const { build: viteBuild } = await import("vite");
+        const { rmSync } = await import("fs");
+        const { fileURLToPath } = await import("node:url");
+        const { createNitro, prepare, copyPublicAssets, build } = await import("nitropack");
+
+        await viteBuild(clientViteConfig());
+
+        const nitro = await createNitro({
+          experimental: {
+            websocket: true
+          },
+          publicAssets: [
+            {
+              baseURL: "/",
+              dir: fileURLToPath(new URL("./dist", import.meta.url)),
+            },
+          ],
+          handlers: [
+            { route: "/**", handler: "#indexHandler" }
+          ],
+          virtual: {
+            "#indexHandler": `
+              import { eventHandler } from 'h3'; 
+              export default eventHandler(() => $fetch('/'));
+            `,
+          }
+        });
+        await prepare(nitro);
+        await copyPublicAssets(nitro);
+        rmSync("dist", { recursive: true, force: true });
+        await build(nitro);
+        await nitro.close();
+      },
+    },
+    start: {
+      run: () => {
+        import(new URL(".output/server/index.mjs", import.meta.url).href);
+      },
+    },
+  },
+});
+
+async function createDevHandler() {
+  const { fromNodeMiddleware, eventHandler } = await import("h3");
+  const { createServer } = await import("vite");
+
+  const viteServer = await createServer({
+    ...clientViteConfig(),
+    server: {
+      middlewareMode: true,
+    },
+  });
+
+  const viteHandler = fromNodeMiddleware(viteServer.middlewares);
+  const router = await createNitroRouter();
+
+  return eventHandler(event => {
+      if (router.isNitroRoute(event)) return;
+      return viteHandler(event);
+  });
+}
+
 async function createNitroRouter() {
   const { default: fg } = (await import("fast-glob"));
-  const rou3 = await import("rou3");
+  const { createRouter, addRoute, findRoute } = await import("rou3");
 
   const extensions = ["js", "ts"];
-  const router = rou3.createRouter();
+  const router = createRouter();
   
   fg.globSync([
     `./routes/**/*.{${extensions.join(",")}}`, 
@@ -30,7 +131,7 @@ async function createNitroRouter() {
     });
 
     routePath = routePath?.length > 0 ? `/${routePath}` : "/";
-    rou3.addRoute(router, undefined, routePath);
+    addRoute(router, undefined, routePath);
   });
 
   const cache = new Set();
@@ -40,109 +141,12 @@ async function createNitroRouter() {
       const pathname = event.path.split("?")[0];
       if (cache.has(pathname)) return true;
       return Boolean(
-        rou3.findRoute(router, event.method, pathname) && 
+        findRoute(router, event.method, pathname) && 
         cache.add(pathname) && 
         true
       );
     }
   };
 }
-
-async function createDevHandler() {
-  const h3 = await import("h3");
-  const vite = await import("vite");
-
-  const viteServer = await vite.createServer({
-    server: {
-      middlewareMode: true
-    },
-    plugins: [solid()]
-  });
-
-  const viteHandler = h3.fromNodeMiddleware(viteServer.middlewares);
-  const router = await createNitroRouter();
-
-  return h3.eventHandler(event => {
-      if (router.isNitroRoute(event)) return;
-      return viteHandler(event);
-  });
-}
-
-const command = defineCommand({
-  subCommands: {
-    dev: {
-      args: {
-        port: {
-          description: "port (default: 3000)",
-          default: 3000,
-        },
-      },
-      run: async ({ args: { port } }) => {
-        const nitropack = await import("nitropack");
-        const devHandler = await createDevHandler();
-        const nitro = await nitropack.createNitro({
-          dev: true,
-          noPublicDir: true, // Leave handling of public dir to vite
-          experimental: {
-            websocket: true
-          },
-          devHandlers: [
-            {
-              route: "/",
-              handler: devHandler
-            }
-          ],
-        });
-        const server = nitropack.createDevServer(nitro);
-        await server.listen(port);
-        await nitropack.prepare(nitro);
-        await nitropack.build(nitro);
-      },
-    },
-    build: {
-      run: async () => {
-        const vite = await import("vite");
-        const fs = await import("fs");
-        const { fileURLToPath } = await import("node:url");
-        const nitropack = await import("nitropack");
-
-        await vite.build({
-          plugins: [solid()]
-        });
-
-        const nitro = await nitropack.createNitro({
-          experimental: {
-            websocket: true
-          },
-          publicAssets: [
-            {
-              baseURL: "/",
-              dir: fileURLToPath(new URL("./dist", import.meta.url)),
-            },
-          ],
-          handlers: [
-            { route: "/**", handler: "#indexHandler" }
-          ],
-          virtual: {
-            "#indexHandler": `
-              import { eventHandler } from 'h3'; 
-              export default eventHandler(() => $fetch('/'));
-            `,
-          }
-        });
-        await nitropack.prepare(nitro);
-        await nitropack.copyPublicAssets(nitro);
-        fs.rmSync("dist", { recursive: true, force: true });
-        await nitropack.build(nitro);
-        await nitro.close();
-      },
-    },
-    start: {
-      run: () => {
-        import(new URL(".output/server/index.mjs", import.meta.url).href);
-      },
-    },
-  },
-});
 
 runMain(command);
